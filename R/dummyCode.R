92236d8a6b46d7788818a97015ef5cd7725a83f2
John Paul Helveston <john.helveston@gmail.com>
2019-04-24 12:11:07 -0400

    Updated dummyCode.R
    Fixed the dummyCode.R function to keep the original variables and the order to restore later after merging

--- R/dummyCode.R
+++ R/dummyCode.R
@@ -12,25 +12,34 @@
 #'     lifeSpan = c(10, 10, 5, 10, 10))
 #' df
 #'
+#' # Create dummy coded variables for the variable "animal":
+#' df.dummy = dummyCode(df, varNames="animal"))
+#' df.dummy
+
 #' # Create dummy coded variables for the variables "animal" and "numLegs":
 #' df.dummy = dummyCode(df, varNames=c("animal", "numLegs"))
 #' df.dummy
-
-dummyCode = function(data, varNames) {
+#'
+dummyCode = function(df, varNames) {
+    df = as.data.frame(df)
+    nonVarNames = colnames(df)[which(! colnames(df) %in% varNames)]
     # Keep the original variables and the order to restore later after merging
-    data$order = seq(nrow(data))
+    df$order = seq(nrow(df))
     for (i in 1:length(varNames)) {
         varName  = varNames[i]
-        colIndex = which(colnames(data) == varName)
-        levels   = sort(unique(data[,colIndex]))
+        colIndex = which(colnames(df) == varName)
+        levels   = sort(unique(df[,colIndex]))
         mergeMat = as.data.frame(diag(length(levels)))
         mergeMat = cbind(levels, mergeMat)
         colnames(mergeMat) = c(varName, paste(varName, levels, sep='_'))
-        data = merge(data, mergeMat)
+        df = merge(df, mergeMat)
     }
-    # Restore the original order
-    data = data[order(data$order),]
-    row.names(data) = data$order
-    data$order <- NULL
-    return(data)
+    # Restore the original column order
+    new = colnames(df)[which(! colnames(df) %in% c(varNames, nonVarNames))]
+    df = df[c(nonVarNames, varNames, new)]
+    # Restore the original row order
+    df = df[order(df$order),]
+    row.names(df) = df$order
+    df$order <- NULL
+    return(df)
 }
