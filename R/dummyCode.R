ab3177bd5aba3109f07e6be2a77a3493a45927f9
John Paul Helveston <john.helveston@gmail.com>
2019-05-10 15:14:11 -0400

    Added jhelvyr.Rproj file and made minor changes to dummyCode() function ("varNames" to just "vars")

--- R/dummyCode.R
+++ R/dummyCode.R
@@ -2,7 +2,7 @@
 #'
 #' This function Creates dummy coded variables in a data frame.
 #' @param data A data frame.
-#' @param varNames The variables in the data frame for which you want to create new dummy coded variables
+#' @param vars The variables in the data frame for which you want to create new dummy coded variables
 #' @export
 #' @examples
 #' # Create an example data frame:
@@ -13,30 +13,30 @@
 #' df
 #'
 #' # Create dummy coded variables for the variable "animal":
-#' df.dummy = dummyCode(df, varNames="animal"))
+#' df.dummy = dummyCode(df, vars="animal"))
 #' df.dummy
 
 #' # Create dummy coded variables for the variables "animal" and "numLegs":
-#' df.dummy = dummyCode(df, varNames=c("animal", "numLegs"))
+#' df.dummy = dummyCode(df, vars=c("animal", "numLegs"))
 #' df.dummy
 #'
-dummyCode = function(df, varNames) {
+dummyCode = function(df, vars) {
     df = as.data.frame(df)
-    nonVarNames = colnames(df)[which(! colnames(df) %in% varNames)]
+    nonVars = colnames(df)[which(! colnames(df) %in% vars)]
     # Keep the original variables and the order to restore later after merging
     df$order = seq(nrow(df))
-    for (i in 1:length(varNames)) {
-        varName  = varNames[i]
-        colIndex = which(colnames(df) == varName)
+    for (i in 1:length(vars)) {
+        var      = vars[i]
+        colIndex = which(colnames(df) == var)
         levels   = sort(unique(df[,colIndex]))
         mergeMat = as.data.frame(diag(length(levels)))
         mergeMat = cbind(levels, mergeMat)
-        colnames(mergeMat) = c(varName, paste(varName, levels, sep='_'))
+        colnames(mergeMat) = c(var, paste(var, levels, sep='_'))
         df = merge(df, mergeMat)
     }
     # Restore the original column order
-    new = colnames(df)[which(! colnames(df) %in% c(varNames, nonVarNames))]
-    df = df[c(nonVarNames, varNames, new)]
+    new = colnames(df)[which(! colnames(df) %in% c(vars, nonVars))]
+    df = df[c(nonVars, vars, new)]
     # Restore the original row order
     df = df[order(df$order),]
     row.names(df) = df$order
