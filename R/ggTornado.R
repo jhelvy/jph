7aeae50b1f4fc5a68af13132d9da77b142045722
John Helveston <john.helveston@gmail.com>
2021-08-24 15:27:54 -0400

    removed dummyCode

--- R/ggTornado.R
+++ R/ggTornado.R
@@ -1,12 +1,13 @@
-#' Creates a "tornado" plot using the tidyverse library
+#' Create a "tornado" plot using ggplot2
 #'
-#' This function creates a "tornado" plot using the tidyverse library.
-#' @param df A data frame of the results of a sensitivity analysis.
+#' This function creates a "tornado" plot using the ggplot2 package. These
+#' are primarily used to display results of a sensitivity analysis.
+#' @param data A data frame containing the results of a sensitivity analysis.
 #' @param baseline The baseline value that the sensitivity analysis is compared to.
-#' @param varName The df column identifying the variable being varied.
-#' @param levelName The df column identifying the sensitivity case level (high or low, relative to the baseline).
-#' @param valName The df column identifying the value of the variable being varied.
-#' @param resultName The df column identifying the result of the output value at the varied variable value.
+#' @param varName The data column identifying the variable being varied.
+#' @param levelName The data column identifying the sensitivity case level (high or low, relative to the baseline).
+#' @param valName The data column identifying the value of the variable being varied.
+#' @param resultName The data column identifying the result of the output value at the varied variable value.
 #' @param xlab The x-axis label (defaults to 'Result').
 #' @param ylab The y-axis label (defaults to 'Parameter').
 #' @export
@@ -17,7 +18,7 @@
 #' # 'level'    = High or Low (relative to the baseline).
 #' # 'varValue' = The value of the variable being varied.
 #' # 'result'   = The result of the output value at the varied variable value.
-#' df = data.frame(
+#' data <- data.frame(
 #'     var      = c('price', 'price', 'fuelEconomy', 'fuelEconomy',
 #'                  'accelTime', 'accelTime'),
 #'     level    = rep(c('high', 'low'), 3),
@@ -29,14 +30,14 @@
 #'
 #' # Make a tornado plot of the sensitivity analysis results:
 #' library(tidyverse)
-#' plot = ggTornado(df=df, baseline=baseline, varName='var',
+#' plot = ggTornado(data = data, baseline=baseline, varName='var',
 #'                  levelName='level', valName='varValue',
 #'                  resultName='result', xlab='Result',
 #'                  ylab='Parameter')
 #' plot
 
 ggTornado = function(
-    df,
+    data,
     baseline,
     varName    = 'var',
     levelName  = 'level',
@@ -46,10 +47,10 @@ ggTornado = function(
     ylab       = 'Parameter') {
 
     # Create a new data frame for plotting
-    newDf = df[c(varName, levelName, valName, resultName)]
-    colnames(newDf) = c('var', 'level', 'val', 'result')
+    df = data[c(varName, levelName, valName, resultName)]
+    colnames(df) = c('var', 'level', 'val', 'result')
 
-    newDf = newDf %>%
+    df = df %>%
         # "Center" the result around the baseline result (so baseline is at 0)
         mutate(result = result - baseline) %>%
         # Compute the range in change from low to high levels for sorting
@@ -57,13 +58,13 @@ ggTornado = function(
         mutate(resultRange = sum(abs(result)))
 
     # Compute labels for the x-axis
-    lb        = floor(10*min(newDf$result))/10
-    ub        = ceiling(10*max(newDf$result))/10
+    lb        = floor(10*min(df$result))/10
+    ub        = ceiling(10*max(df$result))/10
     breaks    = seq(lb, ub, (ub - lb) / 5)
     breakLabs = round(breaks + baseline, 2)
 
     # Make the tornado diagram
-    plot = ggplot(newDf,
+    plot = ggplot(df,
         # Use 'fct_reorder' to order the variables according to shareRange
         aes(x=fct_reorder(var, resultRange), y=result, fill=level)) +
         geom_bar(stat='identity', width=0.6) +
